c8df851c1d0eed355704b350a596ad0b4d79ae6f
Carl Vitullo <carl.vitullo@gmail.com>
2022-05-04 13:31:26 -0400

    Add Discord OAuth

--- app/components/login.tsx
+++ app/components/login.tsx
@@ -1,26 +1,19 @@
-import type { ActionFunction } from "@remix-run/node";
-import { Form } from "@remix-run/react";
+import { Form, useSearchParams } from "@remix-run/react";
 
-import { logout } from "~/models/session.server";
+export function Login({ errors }: { errors?: { [k: string]: string } }) {
+  const [searchParams] = useSearchParams();
+  const redirectTo = searchParams.get("redirectTo") || "/notes";
 
-export const action: ActionFunction = async ({ request }) => {
-  return await logout(request);
-};
-
-export default function Logout({
-  errors,
-}: {
-  errors?: { [k: string]: string };
-}) {
   return (
     <div className="flex min-h-full flex-col justify-center">
       <div className="mx-auto w-full max-w-md px-8">
-        <Form method="post" className="space-y-6">
+        <Form method="post" className="space-y-6" action="/auth">
+          <input type="hidden" name="redirectTo" value={redirectTo} />
           <button
             type="submit"
             className="w-full rounded bg-blue-500  py-2 px-4 text-white hover:bg-blue-600 focus:bg-blue-400"
           >
-            Log out
+            Log in with Discord
           </button>
           <div className="flex items-center justify-between">
             <div className="flex items-center">
--- app/models/user.server.ts
+++ app/models/user.server.ts
@@ -1,14 +1,8 @@
-import bcrypt from "bcryptjs";
 import { randomUUID } from "crypto";
 
 import knex from "~/db.server";
 
-export interface Password {
-  // TODO
-  hash: string;
-}
 export interface User {
-  // TODO
   id: string;
   email: string;
   externalId: string;
@@ -16,56 +10,36 @@ export interface User {
 }
 
 export async function getUserById(id: User["id"]) {
-  return (await knex("users").select())[0] as User;
-  // return (await knex("users").select().where({ id }).limit(1))[0] as User;
+  return knex<User>("users").select().where({ id }).first();
+}
+
+export async function getUserByExternalId(externalId: User["externalId"]) {
+  return await knex<User>("users").select().where({ externalId }).first();
 }
 
 export async function getUserByEmail(email: User["email"]) {
-  return knex("users").select().where({ email });
+  return await knex<User>("users").select().where({ email }).first();
 }
 
 export async function createUser(
   email: User["email"],
-  externalId: User["externalId"]
+  externalId: User["externalId"],
 ) {
-  const out = await knex("users").insert({
-    id: randomUUID(),
-    email,
-    externalId,
-    authProvider: "discord",
-  });
-  return out.at(0);
+  const out = await knex<User>("users")
+    .insert(
+      {
+        id: randomUUID(),
+        email,
+        externalId,
+        authProvider: "discord",
+      },
+      ["id"],
+    )
+    .first();
+  if (!out) throw new Error("Failed to create user!");
+  return out.id;
 }
 
 export async function deleteUserByEmail(email: User["email"]) {
   return knex("users").where({ email }).delete();
 }
-
-export async function verifyLogin(
-  email: User["email"],
-  password: Password["hash"]
-) {}
-//   const userWithPassword = await knex.user.findUnique({
-//     where: { email },
-//     include: {
-//       password: true,
-//     },
-//   });
-
-//   if (!userWithPassword || !userWithPassword.password) {
-//     return null;
-//   }
-
-//   const isValid = await bcrypt.compare(
-//     password,
-//     userWithPassword.password.hash
-//   );
-
-//   if (!isValid) {
-//     return null;
-//   }
-
-//   const { password: _password, ...userWithoutPassword } = userWithPassword;
-
-//   return userWithoutPassword;
-// }
