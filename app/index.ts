d2f001067f3d5c737106c58ee5df6ad815725bf4
Carl Vitullo <carl.vitullo@gmail.com>
2022-05-25 19:17:30 -0400

    Use express to serve app

---
+++ app/index.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import { createRequestHandler } from "@remix-run/express";
+import path from "path";
+import * as build from "@remix-run/dev/server-build";
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+// needs to handle all verbs (GET, POST, etc.)
+app.all(
+  "*",
+  createRequestHandler({
+    // `remix build` and `remix dev` output files to a build directory, you need
+    // to pass that build to the request handler
+    build,
+
+    // return anything you want here to be available as `context` in your
+    // loaders and actions. This is where you can bridge the gap between Remix
+    // and your server
+    getLoadContext(req, res) {
+      return {};
+    },
+  }),
+);
+
+app.listen(process.env.PORT || "3000");
